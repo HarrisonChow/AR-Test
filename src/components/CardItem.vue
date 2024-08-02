cace9440ca0e49ccfa939e856c6229d2e576ec9e
Harrison.Chow <Airwomanzhy@hotmail.com>
2024-08-03 01:15:41 +1000

    add mock event data and list all

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="card">
-        <h3 class="card-title">title</h3>
-        <p class="card-description">description</p>
+        <h3 class="card-title">{{ title }}</h3>
+        <p class="card-description">{{ location }}, {{ eventDate }}</p>
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+    props: {
+        title: String,
+        location: String,
+        eventDate: String,
+    },
+};
 </script>
 
 <style lang="scss"></style>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -5,7 +5,14 @@
             <button>Create New Event</button>
         </div>
         <div class="card-list">
-            <CardItem />
+            <CardItem
+                v-for="card in filteredCards"
+                :key="card.id"
+                :title="card.title"
+                :location="card.location"
+                :eventDate="card.eventDate"
+                :image="card.image"
+            />
         </div>
     </div>
 </template>
@@ -17,7 +24,85 @@ export default {
     components: {
         CardItem,
     },
-    data() {},
+    data() {
+        return {
+            searchQuery: "",
+            cards: [
+                {
+                    id: 1,
+                    title: "Beyond the Valley 2020",
+                    location: "Melbourne",
+                    eventDate: "Sat 29th Nov 2017 ",
+                    image: "https://placehold.co/600x400",
+                },
+                {
+                    id: 2,
+                    title: "Beyond the Valley 2019 Summer Sessions",
+                    location: "Melbourne",
+                    eventDate: "Sat 29th Nov 2017 ",
+                    image: "https://placehold.co/600x400",
+                },
+                {
+                    id: 3,
+                    title: "Beyond the Valley 2018 Spring",
+                    location: "Melbourne",
+                    eventDate: "Sat 29th Nov 2017 ",
+                    image: "https://placehold.co/600x400",
+                },
+                {
+                    id: 4,
+                    title: "Beyond the Valley 2017 Summer",
+                    location: "Melbourne",
+                    eventDate: "Sat 29th Nov 2017 ",
+                    image: "https://placehold.co/600x400",
+                },
+                {
+                    id: 5,
+                    title: "Beyond the Valley Winter Festival 207",
+                    location: "Melbourne",
+                    eventDate: "Sat 29th Nov 2017 ",
+                    image: "https://placehold.co/600x400",
+                },
+                {
+                    id: 6,
+                    title: "Beyond the Valley 2017",
+                    location: "Melbourne",
+                    eventDate: "Sat 29th Nov 2017 ",
+                    image: "https://placehold.co/600x400",
+                },
+                {
+                    id: 7,
+                    title: "Beyond the Valley 2016",
+                    location: "Melbourne",
+                    eventDate: "Sat 29th Nov 2017 ",
+                    image: "https://placehold.co/600x400",
+                },
+                {
+                    id: 8,
+                    title: "Beyond the Valley 2015",
+                    location: "Melbourne",
+                    eventDate: "Sat 29th Nov 2017 ",
+                    image: "https://placehold.co/600x400",
+                },
+                {
+                    id: 9,
+                    title: "Beyond the Valley 2014",
+                    location: "Melbourne",
+                    eventDate: "Sat 29th Nov 2017 ",
+                    image: "https://placehold.co/600x400",
+                },
+            ],
+        };
+    },
+    computed: {
+        filteredCards() {
+            return this.cards.filter((card) =>
+                card.title
+                    .toLowerCase()
+                    .includes(this.searchQuery.toLowerCase())
+            );
+        },
+    },
 };
 </script>
 
